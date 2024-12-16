16788671810e707d707eb84c950da28bbbd624a2
soodongee <151587732+soodongee@users.noreply.github.com>
2024-12-16 13:17:19 -0500

    Update and rename BinTree.cpp to Entry.h

---
+++ Lesson 9/Entry.h
@@ -0,0 +1,25 @@
+#ifndef ENTRY_H_
+#define ENTRY_H_
+
+using namespace std;
+template <typename K, typename V>
+class Entry {
+public: // public functions
+	Entry(const K& k = K(), const V& v = V());// constructor
+	const K& keyy() const; // get key
+	const  V& valuee() const; // get value
+	void setKey(const K& k);  // set key
+	void setValue(const V& v); // set value
+
+
+	friend ostream& operator<<(ostream& os, Entry& p){
+		os<<endl<<"{"<<p._key<<" ; "<<p._value<<"}";
+		return os;
+	}
+
+private: // private data
+	K _key; // key
+	V _value; // value
+};
+
+#endif /* ENTRY_H_ */
