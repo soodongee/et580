f437bfd6a28841b3eff193a82286861578e07184
soodongee <151587732+soodongee@users.noreply.github.com>
2024-12-16 14:26:15 -0500

    Create NodeListG.h

---
+++ Assignment 10/NodeListG.h
@@ -0,0 +1,18 @@
+#include <vector>
+
+template <typename T>
+class NodeListG {
+    
+private:
+    std::vector<T> list; 
+
+public:
+    void push_back(const T& e) { list.push_back(e); } 
+    typename std::vector<T>::iterator begin() { return list.begin(); }
+    typename std::vector<T>::iterator end() { return list.end(); }
+    size_t size() const { return list.size(); }
+    bool empty() const { return list.empty(); }
+    
+};
+
+
