7574e0a0ded9888c385a581c6e0139ecf6d80c2e
soodongee <151587732+soodongee@users.noreply.github.com>
2024-12-16 13:18:16 -0500

    Update main.cpp

--- Lesson 9/main.cpp
+++ Lesson 9/main.cpp
@@ -1,33 +1,65 @@
 #include <iostream>
-#include "BinTree.h"
 
-int main() {
-    BinTree<int> tree;
-    tree.addRoot(10);
-    auto root = tree.root();
-
-    tree.expandExternal(root, 20, 30); // parents 20,30
-
-    auto leftParent = root.left();  // Left parent 20
-    auto rightParent = root.right(); // Right parent 30
-
-    tree.expandExternal(leftParent, 40, 50);   // Childrens of left parent 40, 50
-    tree.expandExternal(rightParent, 60, 70); // Childrens of right parent 60, 70
-
-    auto leftChild1 = leftParent.left();   // Left child of left parent 40
-    auto rightChild1 = leftParent.right(); // Right child of left parent 50
-
-    auto leftChild2 = rightParent.left();  // Left child of right parent 60
-    auto rightChild2 = rightParent.right(); // Right ch ild of right parent 70
+using namespace std;
 
 
-    std::cout << "Root: " << *root << "\n";
+#include "NodeMapG.h"
 
-    std::cout << "Left Parent: " << *leftParent << "\n";
-    std::cout << "Right Parent: " << *rightParent << "\n";
+//#include "DNodeG.h"
+#include "IteratorG.h"
+int main(){
 
-    std::cout << "Children of Left Parent: " << *leftChild1 << ", " << *rightChild1 << "\n";
-    std::cout << "Children of Right Parent: " << *leftChild2 << ", " << *rightChild2 << "\n";
+//	NodeMapG<int,int> mm();
 
-    return 0;
+	NodeMapG<int,int> t1;
+
+	t1.put(8, 5);
+
+	t1.put(2, 5);
+
+	t1.put(3, 7);
+
+	t1.put(2, 3);
+
+	t1.print();
+
+	std::cout<<*t1.find(3)<<endl;
+
+	IteratorG<int,int> it;
+
+	it = t1.find(8);
+
+	std::cout<<*it<<endl;
+
+
+//	Entry<int,int> entry(2,3);
+
+//	t1.put(2,4);
+
+//	IteratorG<int,int> it;
+//	it = t1.put(2,4);
+
+//	std::cout<<*it;
+
+//	t1.erase(2);
+
+
+
+//	Entry<int,int> entry;
+//
+//	entry.setKey(2);
+//	entry.setValue(5);
+//
+//	cout<<entry.valuee();
+//
+//
+//
+//
+//
+//	NodeMapG<int, int> t2;
+
+
+
+
+	return 0;
 }
